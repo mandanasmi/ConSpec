900acd56526d6dc3c6c543b8e529bebbd45abf6a
mandanasmi <samiemandana@gmail.com>
2024-01-15 17:10:06 -0500

    adds arguments to main.py

--- conspecfunction/main.py
+++ conspecfunction/main.py
@@ -3,39 +3,32 @@ import glob
 import os
 import time
 from collections import deque
-
 import torch
-
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import sys
+import numpy as np
+import wandb
 
+########packages to runthe pycolab game
+from tvt import batch_env
+from tvt import nest_utils
+from tvt.pycolab import env as pycolab_env
+
+########packages to run the underlying RL algorithm, mostly identical to https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail
 from a2c_ppo_acktr import algo, utils
 from a2c_ppo_acktr.algo import gail
 from a2c_ppo_acktr.argumentstvt import get_args
 from a2c_ppo_acktr.modelRL import Policy
 from a2c_ppo_acktr.modelConSpec import PolicyCL
-
 from a2c_ppo_acktr.storage import RolloutStorage
 from evaluation import evaluate
-import traceback
-
 from tensorflow.contrib import framework as contrib_framework
-
-from time import process_time
-
-import time
-
-import numpy as np
+import traceback
 from six.moves import range
 from six.moves import zip
 import tensorflow.compat.v1 as tf
-
-from tvt import batch_env
-from tvt import nest_utils
-from tvt.pycolab import env as pycolab_env
-import sys
-import numpy
 
 ###############CONSPEC FUNCTIONS##############################
 from Conspec.moduleConSpec import moduleCL
@@ -43,11 +36,10 @@ from Conspec.conspecfunction import conspecfunctionx
 ##############################################################
 
 
-def main():
-    args = get_args()
-
-    keysUsed = np.zeros((args.head,))
-    goodones = np.zeros((args.head,))
+def main(args):
+    print(args.wandb_group, args.wandb_project, args.exp_name)
+    wandb.init(project=args.wandb_project, name=args.exp_name, tags=['multikeydoor'])
+    wandb.config.update(args)
 
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
@@ -263,4 +255,71 @@ def main():
             pass
 
 if __name__ == "__main__":
-    main()
+    import argparse 
+    from pathlib import Path
+    parser = argparse.ArgumentParser(description='multi-key-door-conspec')
+    parser.add_argument('--num_prototype', type=int, default=8, help='number of prototypes(default: 8)')
+    parser.add_argument('--choiceCLparams', type=int, default=0, help='CL params (default: 0)')
+    parser.add_argument('--expansion', type=int, default=24, help='gail batch size (default: 24)')
+    
+    parser.add_argument('--pycolab_game', default='key_to_doormany4', help='name of the environment (default: key_to_doormany4)')
+    parser.add_argument('--pycolab_apple_reward_min', type=float, default=1., help='pycolab apple reward min (default: 1.)')
+    parser.add_argument('--pycolab_apple_reward_max', type=float, default=2., help='pycolab apple reward max (default: 2.)')
+    parser.add_argument('--pycolab_final_reward', type=float, default=50., help='RMSprop optimizer epsilon (default: 1e-5)')
+    parser.add_argument('--pycolab_num_apples', type=int, default=10, help='gail batch size (default: 128)')
+    parser.add_argument('--pycolab_fix_apple_reward_in_episode', action='store_false', default=True, help='use generalized advantage estimation')
+    parser.add_argument('--pycolab_crop', action='store_true', default=False, help='use generalized advantage estimation')
+    
+    parser.add_argument('--skip', type=int, default=4, help='gail batch size (default: 128)')
+    parser.add_argument('--lrCL', type=float, default=7e-4, help='Conspec learning rate (default: 7e-4)')
+    parser.add_argument('--algo', default='ppoCL', help='algorithm to use: a2c | ppo | acktr')
+    parser.add_argument('--gail', action='store_true', default=False, help='do imitation learning with gail')
+    parser.add_argument('--gail-experts-dir', default='./gail_experts', help='directory that contains expert demonstrations for gail')
+    parser.add_argument('--gail-batch-size', type=int, default=128, help='gail batch size (default: 128)')
+    parser.add_argument('--gail-epoch', type=int, default=5, help='gail epochs (default: 5)')
+    parser.add_argument('--lr', type=float, default=7e-4, help='learning rate (default: 7e-4)')
+    parser.add_argument('--eps', type=float, default=1e-5, help='RMSprop optimizer epsilon (default: 1e-5)')
+    parser.add_argument('--alpha', type=float, default=0.99, help='RMSprop optimizer apha (default: 0.99)')
+    parser.add_argument('--factorR', type=float, default=0.2, help='discount factor for rewards (default: 0.99)')
+    parser.add_argument('--factorC', type=float, default=5000., help='discount factor for rewards (default: 0.99)')
+    parser.add_argument('--gamma', type=float, default=0.99, help='discount factor for rewards (default: 0.99)')
+    parser.add_argument('--use-gae', action='store_true', default=False, help='use generalized advantage estimation')
+    parser.add_argument('--gae-lambda', type=float, default=0.95, help='gae lambda parameter (default: 0.95)')
+    parser.add_argument('--entropy-coef', type=float, default=0.01, help='entropy term coefficient (default: 0.01)')
+    parser.add_argument('--value-loss-coef', type=float, default=0.5, help='value loss coefficient (default: 0.5)')
+    parser.add_argument('--max-grad-norm', type=float, default=0.5, help='max norm of gradients (default: 0.5)')
+
+    parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
+    parser.add_argument('--cuda-deterministic', action='store_true', default=False, help="sets flags for determinism when using CUDA (potentially slow!)")
+    parser.add_argument('--num_iterations', type=int, default=10000, help='Number of iterations (default: %(default)s)')
+    parser.add_argument('--num-processes', type=int, default=16, help='how many training CPU processes to use (default: 16)')
+    parser.add_argument('--num-steps', type=int, default=5, help='number of forward steps in A2C (default: 5)')
+    parser.add_argument('--ppo-epoch', type=int, default=4, help='number of ppo epochs (default: 4)')
+    parser.add_argument('--num-mini-batch', type=int, default=32, help='number of batches for ppo (default: 32)')
+    parser.add_argument('--clip-param', type=float, default=0.2, help='ppo clip parameter (default: 0.2)')
+    parser.add_argument('--log-interval', type=int, default=10, help='log interval, one log per n updates (default: 10)')
+    parser.add_argument('--save-interval', type=int, default=100, help='save interval, one save per n updates (default: 100)')
+    parser.add_argument('--eval-interval', type=int, default=None, help='eval interval, one eval per n updates (default: None)')
+    parser.add_argument('--num-env-steps', type=int, default=10e8, help='number of environment steps to train (default: 10e6)')
+    parser.add_argument('--env_name', default='PongNoFrameskip-v4', help='environment to train on (default: PongNoFrameskip-v4)')
+    parser.add_argument('--log-dir', default='/tmp/gym/', help='directory to save agent logs (default: /tmp/gym)')
+    parser.add_argument('--save-dir', default='trained_models', help='directory to save agent logs (default: ./trained_models/)')
+    parser.add_argument('--no-cuda',action='store_true', default=False, help='disables CUDA training')
+    parser.add_argument('--use-proper-time-limits', action='store_true', default=False, help='compute returns taking into account time limits')
+    parser.add_argument('--recurrent-policy', action='store_false', default=True, help='use a recurrent policy')
+    parser.add_argument('--use-linear-lr-decay', action='store_true', default=False, help='use a linear schedule on the learning rate')
+    parser.add_argument('--output_folder', type=Path, default='output', help='Output folder (default: %(default)s)')
+    parser.add_argument("--wandb_project",type=str,default='gfn-conspec',help="Wandb project name")
+    parser.add_argument("--wandb_group",type=str,default='blake-richards',help="Wandb group name")
+    parser.add_argument("--wandb_dir",type=str,default=f'{os.environ["SCRATCH"]}/exploringConsPec',help="Wandb logdir")
+    parser.add_argument("--exp_name",type=str,default='exp-conspec',help="experiment name")
+    parser.add_argument("--exp_group",type=str,default='exp-conspec',help="conspec")
+    parser.add_argument("--exp_job_type",type=str,default='exp-conspec',help='conspec training')
+
+
+    args = parser.parse_args()
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+    main(args)
+    
+
